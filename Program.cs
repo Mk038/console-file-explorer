bcaacaad4bd3c30309a9626f38bdea4fa18774ba
Idk Idk <masterkochi038@gmail.com>
2023-07-13 10:23:47 -0500

    War Crimes Against Humanity

--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 bool x = true; //keeps the while loop going
 String directoryPath = "C:\\";
 while (x){
     System.Console.WriteLine("Write the file path (C:// is already included and your previous entries)");
     directoryPath += "/"+Console.ReadLine();
-    
-    if (Directory.Exists(directoryPath)){
-            string[] files = Directory.GetFileSystemEntries(directoryPath);
-            foreach (string file in files){
+    if(File.Exists(directoryPath)){
+        ProcessStartInfo startInfo = new()
+        {
+            FileName = "vim",
+            Arguments = directoryPath
+        };
+        Process p = Process.Start(startInfo);
+        p.WaitForExit();
+    }else if(Directory.Exists(directoryPath)){
+        string[] files = Directory.GetFileSystemEntries(directoryPath);
+        foreach (string file in files){
             System.Console.WriteLine(file);
         }
     }
